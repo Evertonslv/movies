43079aaf4d87eb0d2c47d9eada440b609ebb88de
Everton Da Silva <everton@Everton>
2023-11-15 15:43:18 -0300

    change config typeorm

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,11 +1,20 @@
+import dotenv from 'dotenv'
 import { DataSource } from 'typeorm'
+
+const environment = process.env.NODE_ENV
+const envFile = `.env.${environment}`
+
+dotenv.config({ path: envFile })
 
 export const connection = new DataSource({
   migrationsTableName: 'migrations',
   type: 'sqlite',
-  database: 'src/database/golden-raspberry-awards.db',
+  database: process.env.TYPEORM_DATABASE ?? 'database/db/golden-raspberry-awards.db',
   logging: false,
   synchronize: false,
-  entities: ['src/models/**.ts'],
-  migrations: ['src/database/migrations/*{.ts,.js}']
+  entities: [
+    'src/infra/entities/**.ts',
+    'dist/infra/entities/**.js'
+  ],
+  migrations: ['database/migrations/*{.ts,.js}']
 })
